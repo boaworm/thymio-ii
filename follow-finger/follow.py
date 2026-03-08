be63931f3b21caf8486385f882ca22c896441032
Henrik Thorburn <boaworm@gmail.com>
2026-03-07 19:33:19 -0600

    ...

--- follow-finger/follow.py
+++ follow-finger/follow.py
@@ -21,17 +21,79 @@ async def run_thymio():
         # 2. Access the array (Python lists start at index 0)
         sensors = node.v.prox.horizontal
         
-        # 3. Logic: If center sensor > 1000, move forward
-        if sensors[2] > 1000:
-            speed = 200
-        else:
-            speed = 0
-            
-        # 4. Send the speed to the motors
+        speed = 0
         v = {
             "motor.left.target": [speed],
             "motor.right.target": [speed]
         }
+
+        ## First we find the sensor with the highest value (closest object)
+        max_val = 0
+        max_sensor_index = -1
+        move_forward = False
+        move_backwards = False
+
+        for i, val in enumerate(node.v.prox.horizontal):
+            if val > max_val:
+                max_val = val
+                max_sensor_index = i
+
+                ## Any value is greater than 3500, we need to move back 
+                if val > 3500:
+                    move_forward = False
+                    move_backwards = True
+                elif val > 0 and move_backwards == False:
+                    move_forward = True
+
+    
+        ## At this point, we know that
+        if move_backwards:
+            speed = 50
+            v["motor.left.target"] = [-speed]
+            v["motor.right.target"] = [-speed]
+        elif move_forward:
+            speed = 50
+
+            if max_sensor_index == 0 or max_sensor_index == 1:
+                v["motor.left.target"] = [-speed]
+                v["motor.right.target"] = [speed]
+            elif max_sensor_index == 3 or max_sensor_index == 4:
+                v["motor.left.target"] = [speed]
+                v["motor.right.target"] = [-speed]
+            else: 
+                ## Middle sensor is closest. 
+                ## We may still need some finetuning
+                sensor_1 = node.v.prox.horizontal[1]
+                sensor_3 = node.v.prox.horizontal[3]
+
+                if abs(sensor_1 - sensor_3) > 50:
+                    # Need some micro adjustments 
+                    if sensor_1 > sensor_3:
+                        # If left sensor is much stronger, turn left
+                        v["motor.left.target"] = [-speed]
+                        v["motor.right.target"] = [speed]
+                    else:
+                        # If right sensor is much stronger, turn right
+                        v["motor.left.target"] = [speed]
+                        v["motor.right.target"] = [-speed]
+                else:
+                    # Otherwise, move forward or backwards
+                    if sensors[2] > 0:
+                        if sensors[2] > 4000:
+                            speed = -speed # Move backward if too close
+                        elif sensors[2] > 3500:
+                            ## Stay still 
+                            speed = 0
+
+                    # Send the same speed to both motors
+                    v = {
+                        "motor.left.target": [speed],
+                        "motor.right.target": [speed]
+                    }
+                        
+
+
+        # Execute movement
         await node.set_variables(v)
         
         # Small sleep to let the robot breathe
